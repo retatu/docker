1819e592dde58bb5ec1a1b571919c7f1efec7276
Matheus <mrehbein45@gmail.com>
2021-12-31 07:09:17 -0300

    integrating mysql and nodejs

--- section-4/node/index.js
+++ section-4/node/index.js
@@ -1,7 +1,18 @@
 const express = require ('express')
 const app = express()
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+const sql = `INSERT INTO people (name) values('MATHEUS')`;
+connection.query(sql)
+connection.end()
 
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res)=> {
   res.send('<h1>Full cycle123</h1>');
